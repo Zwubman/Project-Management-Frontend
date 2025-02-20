4813feb4bff4b192e0952e07413d63ee637e534b
Zwubman <zwubeman@gmail.com>
2025-02-20 04:32:58 -0500

    routing fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
   return (
     <div className=" bg-gray-900 text-white h-screen p-2 relative">
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
